73e51295204c0bcc4bd8de87d8e5a8cceb4f4610
Ryan Hageman <improv42@gmail.com>
2016-03-09 20:45:10 -0600

    Add user and tweet validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ActiveRecord::Base
+  validates :message, presence: true, length: { maximum: 140, too_long: "A tweet can only be 140 characters, max. Everybody knows that!" }
+
   belongs_to :user
 end
